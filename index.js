0391f91cde33d3df139cd6782c686f5dac7a0d29
сережа 🧟‍♂️ <glavnipopivy@MacBook-Air-sereza-2.local>
2024-12-28 01:05:23 +0300

    popitka3

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+require('dotenv').config();
+
 const apiKey = process.env.API_KEY;
 const dbPassword = process.env.DATABASE_PASSWORD;
 
 console.log(`Using API key: ${apiKey}`);
+console.log(`Using database password: ${dbPassword}`);
